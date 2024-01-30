4eed2ab9986eee43d9afac92b666e6965c6b6c50
San-em-Rede <126499089+San-em-Rede@users.noreply.github.com>
2024-01-30 12:28:29 -0400

    Mudanças #2

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-$(document).ready(function()){
-    $('form').on ('submit, funciton(e))
-}
+$(document).ready(function(){
+    $('form').on ('submit', funciton(e){
+        e.preventDefault();
+        const NomeDaTarefa = $('input').val();
+        const novaTarefa = $(`<li>${nomeDaTarefa}</li>`)
+
+        $(novaTarefa).appendTo('ul')
+        $('input').val('')
+})
+
+    $('ul').on('click', 'li', function(e){
+    e.preventDefault();
+    $(this).toggleClass('riscarTarefa')
+
+})
+
+
+})
